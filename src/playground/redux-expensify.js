57d38da55b02f52a1474ed857db6ace52717714d
Kirankumar Ahir <kirankatariya8778@gmail.com>
2018-10-22 23:20:17 +0530

    Adding redux configurations and relevant files

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -65,28 +65,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
     };
 };
 
-const setTextFilter = (text = '') => ({
-    type: 'SET_TEXT_FILTER',
-    text
-});
 
-const sortByAmount = () => ({
-    type: 'SORT_BY_AMOUNT'
-});
-
-const sortByDate = () => ({
-    type: 'SORT_BY_DATE'
-});
-
-const setStartDate = (startDate) => ({
-    type: 'SET_START_DATE',
-    startDate
-});
-
-const setEndDate = (endDate) => ({
-    type: 'SET_END_DATE',
-    endDate
-});
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {   
