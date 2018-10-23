bc589170c69aacce2b06072b70bc594105c93245
Kirankumar Ahir <kirankatariya8778@gmail.com>
2018-10-23 23:05:15 +0530

    Adding date filters

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -61,9 +61,9 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ error: '' }));
             this.props.onSubmit({
                 description: this.state.description,
-                amount: parseFloat(this.state.amount, 10) * 100,
+                amount: (parseFloat(this.state.amount)*100).toFixed(),
                 createdAt: this.state.createdAt.valueOf(),
-                note: this.state.note
+                note: this.state.note,
             });
         }
     };
