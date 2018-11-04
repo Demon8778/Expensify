41d639129d54d9bff461657a488304259355fbe8
Kirankumar Ahir <kirankatariya8778@gmail.com>
2018-11-04 16:22:18 +0530

    Fetching data at starting of the app

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters'
+import { startSetExpenses } from './actions/expenses';
 import getVisibleExpenses from './selectors/expenses';
 import './firebase/firebase';
 import 'normalize.css/normalize.css';
@@ -36,4 +35,9 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
