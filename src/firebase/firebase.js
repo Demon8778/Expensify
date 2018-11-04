f91741b68004971973ddfad9d432c9c522beafcf
Kirankumar Ahir <kirankatariya8778@gmail.com>
2018-11-04 15:07:17 +0530

    Adding database cofiguration for both test and development

---
+++ src/firebase/firebase.js
@@ -0,0 +1,62 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+// database.ref('expenses')
+//     .on('value', (snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSanpshot) => {
+//             expenses.push({
+//                 id: childSanpshot.key,
+//                 ...childSanpshot.val()
+//             });
+//         });
+
+//         console.log(expenses);
+//     });
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSanpshot) => {
+//             expenses.push({
+//                 id: childSanpshot.key,
+//                 ...childSanpshot.val()
+//             });
+//         });
+
+//         console.log(expenses);
+//     });
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 109500,
+//     createdAt: 9785412134546
+// });
+
+// database.ref('expenses').push({
+//     description: 'Phone bill',
+//     note: '',
+//     amount: 5900,
+//     createdAt: 9785412134546
+// });
+
+// database.ref('expenses').push({
+//     description: 'Water bill',
+//     note: '',
+//     amount: 1200,
+//     createdAt: 9785412134546
+// });
